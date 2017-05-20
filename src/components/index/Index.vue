da7aaa4a62a6f2cd384bd3138dff6e1a9f28dade
xinghua.wen <xinghua.wen@acorn-net.com>
2017-05-20 18:06:20 +0800

    add support to sass & config

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,6 +1,8 @@
 <template lang="jade">
   div.container
     header
+      ul.navbar
+        li.navbar-item(v-for="item in navbar")(v-bind:class="{ active: item.checked }"){{item.text}}
     div.content
       div.title {{welInfo}}
     footer
@@ -10,13 +12,32 @@ export default {
   name: 'index',
   data () {
     return {
-      welInfo: 'Welcome to my hotel App'
+      welInfo: 'Welcome to my hotel App',
+      navbar: [{text: '首页', checked: true},
+              {text: '详细数据', checked: false},
+              {text: '我的信息', checked: false}]
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
+ ul, li {
+   list-style: none;
+ }
+</style>
 
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  justify-content: row flex-start;
+}
+.navbar-item {
+  width: calc(100% /3);
+}
+.navbar-item.active {
+  color: #FF5252;
+  font-size: 16px;
+}
 </style>
 
