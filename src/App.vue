3bccc04372815260b6a132cc8cc3f9f738ea665a
xinghua.wen <xinghua.wen@acorn-net.com>
2017-05-23 11:42:50 +0800

    add swipe button to use

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     //- router-view
     div.container
       app-header
-      v-touch.hotel_content(v-on:swipeleft="onSwipeLeft" v-on:swiperight="swiperight")
+      div.turn_left.iconfont
+        a(@click="onSwiperight")
+      div.turn_right.iconfont
+        a(@click="onSwipeLeft")
+      v-touch.hotel_content(v-on:swipeleft="onSwipeLeft" v-on:swiperight="onSwiperight")
         router-view
       app-footer
 </template>
@@ -28,7 +32,7 @@ export default {
       this.$store.dispatch('addNavbarIndex')
       this.$router.push(this.navbar.initData[this.navbar.index].path)
     },
-    swiperight: function () {
+    onSwiperight: function () {
       this.$store.dispatch('subNavbarIndex')
       this.$router.push(this.navbar.initData[this.navbar.index].path)
     }
@@ -45,9 +49,50 @@ export default {
   color: #2c3e50;
   margin-top: 20px;
 }
+
+/**
+* 引入css基础框架bootstrap和自定义css基础框架
+*/
+@import './assets/styles/plugins/bootstrap/bootstrap.scss';
+@import './assets/styles/custommade/main.scss';
+</style>
+
+<style lang="scss" scoped>
+$hori-offset: -1em;
+
+%div_turn {
+  position: absolute;
+  top: 50%;
+}
+
+%div_turn_pseudo {
+  width: 20px;
+  height: 20px;
+}
+
 .hotel_content {
   margin-top: 30px;
 }
-@import './assets/styles/plugins/bootstrap/bootstrap.scss';
-@import './assets/styles/custommade/main.scss';
+
+.turn_left {  
+  @extend %div_turn;
+  left: $hori-offset;
+  a {
+    @extend %div_turn_pseudo;
+    &:before{
+      content: "\e628";
+    }
+  }
+}
+
+.turn_right {
+  @extend %div_turn;
+  right: $hori-offset;
+  a {
+    @extend %div_turn_pseudo;
+    &:after{
+      content: "\e642";
+    }
+  } 
+}
 </style>
